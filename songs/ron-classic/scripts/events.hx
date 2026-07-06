e9454033eee4b4cd3573d1904d73e2a20f889ba8
HELLISH666-warframe <102866355+HELLISH666-warframe@users.noreply.github.com>
2026-07-06 14:04:48 +1000

    Ron-Classic.

--- songs/ron-classic/scripts/events.hx
+++ songs/ron-classic/scripts/events.hx
@@ -1,1 +1,9 @@
 function postCreate() iconP1.setIcon('oldbf');
+
+function beatHit(curBeat) {
+	switch(curBeat) {
+		case 35:FlxTween.tween(FlxG.camera, {zoom: 1.5}, 0.4, {ease: FlxEase.expoOut});
+		dad.playAnim('cheer', true);
+		case 79|143|147|155|167|182:FlxTween.tween(FlxG.camera, {zoom: 1.5}, 0.4, {ease: FlxEase.expoOut});
+	}
+}
